9a6f6d034e12b732f7c996e3897f178a93036808
KaliLinuxAbuser <136770573+JayceProgramming@users.noreply.github.com>
2024-05-21 13:10:25 -0400

    Add files via upload

---
+++ HAMBURGER.js
@@ -0,0 +1,10 @@
+function expandMenu() {
+	<!--document.getElementById("menu").classList.add("open");-->
+	document.getElementById("menu").style.display = "block";
+}
+
+function collapseMenu() {
+	document.getElementById("menu").classList.remove("open");
+	document.getElementById("menu").style.display = "none";
+}
+		
